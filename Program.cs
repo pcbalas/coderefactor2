fcaa29a677282910fe21b0e0a50f1dcf0898c7bc
pcbalas <pcbalas@gmail.com>
2024-10-24 10:15:11 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace coderefactor2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            var result = class1.Calculate(1000, 2, 5);
+
+            System.Console.WriteLine(result);
+        }
+    }
+}
